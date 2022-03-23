9c644601369b55b8c9eef6427b03dcec8763fa93
Alina Saratova <54868058+asaratova@users.noreply.github.com>
2022-03-22 22:17:35 -0700

    this should work if you change headers

--- requestingData.py
+++ requestingData.py
@@ -1,37 +1,44 @@
-#Creating a basis for scraping
-
+# Creating a basis for scraping
 import urllib.request
-req = urllib.request.Request(url, headers=headers)
-resp = urllib.request.urlopen(req)
-content = resp.read()
-import time
-import csv
+import scrapeData
 
-#this may give blank pages
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
 import requests
+from requests.packages.urllib3.util import Retry
+from requests.adapters import HTTPAdapter
+import csv
+import time
+
+
+# this may give blank pages
+
 session = requests.Session()
 retry = Retry(connect=7, backoff_factor=5)
 adapter = HTTPAdapter(max_retries=retry)
 session.mount('http://', adapter)
 session.mount('https://', adapter)
 
-#insert header here like: headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'}
+# insert header here like: headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'}
+headers = {'user-agent': 'Chrome/99.0.4844.82 (Lenovo; IdeaPad 5 15IIL05)'}
+url = 'https://archiveofourown.org/tags/Bill%20Nye%20the%20Science%20Guy/works'
+
+req = urllib.request.Request(url, headers=headers)
+resp = urllib.request.urlopen(req)
+content = resp.read()
+
 
 def getContent(url, start_page=1, end_page=1):
-    basic_url = url 
-    #should be of the form: "https://archiveofourown.org/tags/###TAG###/works?page="
-    
+    basic_url = url
+    # should be of the form: "https://archiveofourown.org/tags/###TAG###/works?page="
+
     for i in range(start_page, end_page+1):
         url = basic_url+str(i)
         try:
-            req = urllib.request.Request(url,headers=headers)
+            req = urllib.request.Request(url, headers=headers)
             resp = urllib.request.urlopen(req)
             pageName = "./SomeContent/"+str(i)+".html"
             with open(pageName, 'w') as f:
-                f.write(resp.read().decode('utf-8'))        
-                print (pageName, end=" ")
+                f.write(resp.read().decode('utf-8'))
+                print(pageName, end=" ")
             time.sleep(5)
         except urllib.error.HTTPError as e:
             if e.code == 429:
@@ -40,11 +47,12 @@ def getContent(url, start_page=1, end_page=1):
                 print('we should restart with this url:', url)
                 break
             raise
-            
-totalPages = ###
+
+
+totalPages = 0
 for i in range(1, totalPages+1):
     pageName = "./SomeContent/"+str(i)+".html"
     with open(pageName, mode='r', encoding='utf8') as f:
         print('Now we are opening page', i, '...')
         page = f.read()
-        process_basic(page)
+        scrapeData.process_basic(page)
--- scrapeData.py
+++ scrapeData.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
 import re
+import csv
+import time
 
-header = ['Title', 'Author', 'ID', 'Date_updated', 'Rating', 'Pairing', 'Warning', 'Complete', 'Language', 'Word_count', 'Num_chapters', 'Num_comments', 'Num_kudos', 'Num_bookmarks', 'Num_hits']
-with open('SomeName.csv','w', encoding='utf8') as f:
+header = ['Title', 'Author', 'ID', 'Date_updated', 'Rating', 'Pairing', 'Warning', 'Complete',
+          'Language', 'Word_count', 'Num_chapters', 'Num_comments', 'Num_kudos', 'Num_bookmarks', 'Num_hits']
+with open('SomeName.csv', 'w', encoding='utf8') as f:
     writer = csv.writer(f)
     writer.writerow(header)
-    
+
 
 def process_basic(page_content):
     bs = BeautifulSoup(page_content, 'lxml')
@@ -25,74 +28,82 @@ def process_basic(page_content):
     bookmarks = []
     hits = []
 
-    for article in bs.find_all('li', {'role':'article'}):
-        titles.append(article.find('h4', {'class':'heading'}).find('a').text)
+    for article in bs.find_all('li', {'role': 'article'}):
+        titles.append(article.find('h4', {'class': 'heading'}).find('a').text)
         try:
-            authors.append(article.find('a', {'rel':'author'}).text)
+            authors.append(article.find('a', {'rel': 'author'}).text)
         except:
             authors.append('Anonymous')
-        ids.append(article.find('h4', {'class':'heading'}).find('a').get('href')[7:])
-        date_updated.append(article.find('p', {'class':'datetime'}).text)
-        ratings.append(article.find('span', {'class':re.compile(r'rating\-.*rating')}).text)
-        pairings.append(article.find('span', {'class':re.compile(r'category\-.*category')}).text)
-        warnings.append(article.find('span', {'class':re.compile(r'warning\-.*warnings')}).text)
-        complete.append(article.find('span', {'class':re.compile(r'complete\-.*iswip')}).text)
-        languages.append(article.find('dd', {'class':'language'}).text)
-        count = article.find('dd', {'class':'words'}).text
+        ids.append(article.find('h4', {'class': 'heading'}).find(
+            'a').get('href')[7:])
+        date_updated.append(article.find('p', {'class': 'datetime'}).text)
+        ratings.append(article.find(
+            'span', {'class': re.compile(r'rating\-.*rating')}).text)
+        pairings.append(article.find(
+            'span', {'class': re.compile(r'category\-.*category')}).text)
+        warnings.append(article.find(
+            'span', {'class': re.compile(r'warning\-.*warnings')}).text)
+        complete.append(article.find(
+            'span', {'class': re.compile(r'complete\-.*iswip')}).text)
+        languages.append(article.find('dd', {'class': 'language'}).text)
+        count = article.find('dd', {'class': 'words'}).text
         if len(count) > 0:
             word_count.append(count)
         else:
             word_count.append('0')
-        chapters.append(article.find('dd', {'class':'chapters'}).text.split('/')[0])
+        chapters.append(article.find(
+            'dd', {'class': 'chapters'}).text.split('/')[0])
         try:
-            comments.append(article.find('dd', {'class':'comments'}).text)
+            comments.append(article.find('dd', {'class': 'comments'}).text)
         except:
             comments.append('0')
         try:
-            kudos.append(article.find('dd', {'class':'kudos'}).text)
+            kudos.append(article.find('dd', {'class': 'kudos'}).text)
         except:
             kudos.append('0')
         try:
-            bookmarks.append(article.find('dd', {'class':'bookmarks'}).text)
+            bookmarks.append(article.find('dd', {'class': 'bookmarks'}).text)
         except:
             bookmarks.append('0')
         try:
-            hits.append(article.find('dd', {'class':'hits'}).text)
+            hits.append(article.find('dd', {'class': 'hits'}).text)
         except:
             hits.append('0')
 
-    df = pd.DataFrame(list(zip(titles, authors, ids, date_updated, ratings, pairings,\
-                              warnings, complete, languages, word_count, chapters,\
+    df = pd.DataFrame(list(zip(titles, authors, ids, date_updated, ratings, pairings,
+                               warnings, complete, languages, word_count, chapters,
                                comments, kudos, bookmarks, hits)))
-    
+
     print('Successfully processed', len(df), 'rows!')
-    
-    with open('SomeName.csv','a', encoding='utf8') as f:
+
+    with open('SomeName.csv', 'a', encoding='utf8') as f:
         df.to_csv(f, header=False, index=False)
     temp = pd.read_csv('SomeName.csv')
     print('Now we have a total of', len(temp), 'rows of data!')
     print('================================')
-    
-    
+
     def get_tags(article):
-    tags = []
-    for child in article.find('ul', {'class':'tags commas'}).children:
-        if isinstance(child, NavigableString):
-            pass
-        else:
-            tags.append(child.text.strip())
-    return ', '.join(tags)
+        tags = []
+        for child in article.find('ul', {'class': 'tags commas'}).children:
+            if isinstance(child, NavigableString):
+                pass
+            else:
+                tags.append(child.text.strip())
+        return ', '.join(tags)
+
 
 def get_summary(article):
     try:
-        out = article.find('blockquote', {'class':'userstuff summary'}).text.strip()
+        out = article.find(
+            'blockquote', {'class': 'userstuff summary'}).text.strip()
         return out
     except:
         return ''
-        
+
 
 def open_fic(work_id, headers):
-    url = 'https://archiveofourown.org' + work_id + '?view_adult=true&show_comments=true&view_full_work=true'
+    url = 'https://archiveofourown.org' + work_id + \
+        '?view_adult=true&show_comments=true&view_full_work=true'
     req = urllib.request.Request(url, headers=headers)
     resp = urllib.request.urlopen(req)
     print('Successfully opened fiction:', url)
@@ -101,8 +112,11 @@ def open_fic(work_id, headers):
     return bs
 
 # when there's one or none page of comments
+
+
 def compile_comments(bs):
-    comments = bs.find_all('li', {'class':'comment group odd'}) # only include comments on the first level (no replies)
+    # only include comments on the first level (no replies)
+    comments = bs.find_all('li', {'class': 'comment group odd'})
     l_comments = []
     if len(comments) > 0:
         for i in comments:
@@ -112,12 +126,16 @@ def compile_comments(bs):
         return ''
 
 # when there are 2+ pages of comments
-def turn_page_comments(bs, work_id, headers, start_page = 1):
-    page_limit = int(bs.find('li', {'class':'next'}).previous_sibling.previous_sibling.text)
+
+
+def turn_page_comments(bs, work_id, headers, start_page=1):
+    page_limit = int(
+        bs.find('li', {'class': 'next'}).previous_sibling.previous_sibling.text)
     print('** We have a total of', page_limit, 'pages of comments')
     out = ''
     for i in range(start_page, page_limit+1):
-        url = 'https://archiveofourown.org' + work_id + '?page=' + str(i) + '&show_comments=true&view_adult=true&view_full_work=true'
+        url = 'https://archiveofourown.org' + work_id + '?page=' + \
+            str(i) + '&show_comments=true&view_adult=true&view_full_work=true'
         try:
             req = urllib.request.Request(url, headers=headers)
             resp = urllib.request.urlopen(req)
